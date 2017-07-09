75a87824e521c01a73d0ec24205e8e5a09d4bf2f
Martin Dennhardt <martin@dennhardt.de>
2017-07-09 16:54:40 +0200

    Add multithreading

--- StaticHttpServer/HttpServer.cs
+++ StaticHttpServer/HttpServer.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace StaticHttpServer
 {
+    public class ConnectionState
+    {
+        public Socket Handler;
+        public readonly byte[] Buffer = new byte[64];
+        public string Data;
+    }
     /// <summary>
     /// Yes, I know of TcpListener. This class implements the functionality
     /// itself, because it's in the spirit of the challenge.
@@ -13,66 +21,87 @@ namespace StaticHttpServer
     public class HttpServer : IDisposable
     {
         private const string DefaultPage = "index.html";
-        private readonly int _maxNumConnections;
         private readonly Socket _listener = new Socket(SocketType.Stream, ProtocolType.Tcp);
         private readonly string _hostIp;
         private readonly int _port;
         private readonly Uri _documentRoot;
+        private static readonly ManualResetEvent Semaphore = new ManualResetEvent(true);
 
         public HttpServer(string hostIp, int port, string documentRoot, int maxNumConnections = 10)
         {
             _port = port;
             _documentRoot = new Uri(documentRoot);
-            _maxNumConnections = maxNumConnections;
             _hostIp = hostIp;
             _listener.Bind(new IPEndPoint(IPAddress.Parse(_hostIp), port));
+            _listener.Listen(maxNumConnections);
         }
 
         public void Start()
         {
-            _listener.Listen(_maxNumConnections);
+            Console.WriteLine("Waiting for Connections at {0}:{1}", _hostIp, _port);
             while (true)
             {
-                Console.WriteLine("Waiting for Connection at {0}:{1}", _hostIp, _port);
-                using (var handler = _listener.Accept())
+                Semaphore.Reset();
+                _listener.BeginAccept(AcceptCallback, _listener);
+                Semaphore.WaitOne();
+            }
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            Semaphore.Set();
+            var listener = (Socket) ar.AsyncState;
+            var handler = listener.EndAccept(ar);
+            var state = new ConnectionState {Handler = handler};
+            handler.BeginReceive(state.Buffer, 0, state.Buffer.Length, SocketFlags.None, ReceiveCallback, state);
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            var state = (ConnectionState) ar.AsyncState;
+            var handler = state.Handler;
+            var fragmentLength = handler.EndReceive(ar);
+            state.Data += Encoding.ASCII.GetString(state.Buffer, 0, fragmentLength);
+            if (!state.Data.EndsWith("\r\n\r\n"))
+            {
+                handler.BeginReceive(state.Buffer, 0, state.Buffer.Length, SocketFlags.None, ReceiveCallback, state);
+            }
+            else
+            {
+                var request = HttpRequest.Parse(state.Data);
+                Console.WriteLine("RECV {0} {1} {2} {3}",
+                    handler.RemoteEndPoint,
+                    request.Method,
+                    request.Path,
+                    request.Version);
+                var file = new Uri(Path.Combine(_documentRoot.AbsolutePath,
+                    request.Path.TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)));
+                if (!_documentRoot.IsBaseOf(file))
                 {
-                    HandleConnection(handler);
+                    Send(handler, "HTTP/1.0 403 Forbidden");
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(Path.GetFileName(file.AbsolutePath)))
+                    {
+                        file = new Uri(Path.Combine(file.AbsolutePath, DefaultPage));
+                    }
+                    SendFile(handler, file.AbsolutePath);
                 }
             }
         }
 
-        private void HandleConnection(Socket handler)
+        private static void Send(Socket handler, string message)
         {
-            string requestString = null;
-            while (true)
-            {
-                var fragment = new byte[1024];
-                var fragmetLength = handler.Receive(fragment);
-                requestString += Encoding.ASCII.GetString(fragment, 0, fragmetLength);
-                if (requestString.EndsWith("\r\n\r\n"))
-                {
-                    break;
-                }
-            }
-            var request = HttpRequest.Parse(requestString);
-            Console.WriteLine("Connection from {0} {1} {2} {3}",
-                handler.RemoteEndPoint,
-                request.Method,
-                request.Path,
-                request.Version);
-            var file = new Uri(Path.Combine(_documentRoot.AbsolutePath, request.Path.TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)));
-            if (!_documentRoot.IsBaseOf(file))
-            {
-                handler.Send(Encoding.ASCII.GetBytes("HTTP/1.0 403 Forbidden"));
-            }
-            else
-            {
-                if (string.IsNullOrEmpty(Path.GetFileName(file.AbsolutePath)))
-                {
-                    file = new Uri(Path.Combine(file.AbsolutePath, DefaultPage));
-                }
-                SendFile(handler, file.AbsolutePath);
-            }
+            var data = Encoding.ASCII.GetBytes(message);
+            handler.BeginSend(data, 0, data.Length, SocketFlags.None, SendCallback, handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            var handler = (Socket) ar.AsyncState;
+            handler.EndSend(ar);
+            handler.Shutdown(SocketShutdown.Both);
             handler.Close();
         }
 
@@ -85,7 +114,7 @@ namespace StaticHttpServer
             }
             else
             {
-                handler.Send(Encoding.ASCII.GetBytes("HTTP/1.0 404 Not Found"));
+                Send(handler, "HTTP/1.0 404 Not Found");
             }
         }
 
@@ -95,7 +124,7 @@ namespace StaticHttpServer
             data += $"Content-Type: {contentType}\r\n";
             data += "\r\n";
             data += $"{content}\r\n";
-            handler.Send(Encoding.ASCII.GetBytes(data));
+            Send(handler, data);
         }
 
         public void Dispose()
